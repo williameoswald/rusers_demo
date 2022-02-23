f94a58326f514c37eb3675483abc7364218b7152
Will Oswald <57539054+williameoswald@users.noreply.github.com>
2022-02-23 14:21:26 +0000

    Create 1_assemble_data.R

---
+++ 1_assemble_data.R
@@ -0,0 +1,8 @@
+# title: Data Preparation
+
+# ------------------------------------------------------------
+# LOAD DATASETS
+# ------------------------------------------------------------
+
+# Load randomisation datasets
+df <- read_excel(here("data","dataset.xlsx")) 
